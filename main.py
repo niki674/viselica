8a5289ecee36eea1e6f5edea1b10ce6126c2561b
Nikita Kalchenko <nikitakalcenko9@gmail.com>
2023-11-06 18:07:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+# clear = lambda: os.system('cls')
+
+print('Привет! Я загадал случайное слово, твоя задача - угадать его!')
+print('Поехали!')
+
+words = {'коса', 'пила', 'мыло', 'окно', 'суши', 'дыра', 'лапа', 'заяц'}
+word = words.pop()
+
+letters = []
+isWin = True
+attempts = 6
+
+while attempts > 0:
+    isWin = True
+    letter = input('Введите букву: ')
+    # clear()
+    letters.append(letter)
+    for simb in word:
+        if simb in letters:
+            print(simb, end=' ')
+        else:
+            print('*', end=' ')
+            isWin = False
+    print()
+
+    if isWin:
+        print('Ты выиграл! Мои поздравления!')
+        break
+
+    if letter not in word:
+        attempts -= 1
+        print(f'у тебя осталось {attempts} попыток.')
+        if attempts <= 0:
+            print('Ты проиграл')
